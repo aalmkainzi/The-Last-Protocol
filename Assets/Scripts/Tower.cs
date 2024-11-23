58ec21ef4896242f6fa20fbeaf680b5761f7e412
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2024-11-23 20:34:41 +0300

    changes

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    public int price;
     public int health;
+    public float attackRange;
+    public float power;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
