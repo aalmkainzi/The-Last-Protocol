d4ab910a0d022de6b5e300b877cadf72c2548c08
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2024-11-26 18:53:03 +0300

    added navmesh agents, the basic movement works

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,8 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,11 +12,29 @@ public class Enemy : MonoBehaviour
     public int power;
     Animator animator;
     RadarTower radarTower;
-
+    NavMeshAgent agent;
+    public bool flying;
+    // Rigidbody rb;
     void Start()
     {
-        animator = GetComponent<Animator>();
+        // rb = GetComponent<Rigidbody>();
         radarTower = GameObject.FindWithTag("radioTower").GetComponent<RadarTower>();
+        agent = GetComponent<NavMeshAgent>();
+
+        if (flying)
+        {
+            animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+            float seed = Random.Range(1.25f, 3.5f);
+            Transform flyer = transform.GetChild(0);
+            flyer.DOMoveY(flyer.position.y + seed, Random.Range(2.0f, 4.0f), false).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        }
+        else
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        agent.SetDestination(radarTower.gameObject.transform.position);
+
     }
 
     void Update()
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using static UnityEditor.PlayerSettings;
 
 public class EnemySpanwer : MonoBehaviour
 {
@@ -20,11 +21,24 @@ public class EnemySpanwer : MonoBehaviour
 
     IEnumerator SpawnWaveLoop(EnemyWave wave)
     {
+        GameObject prefab = gameplayManager.enemyPrefabs[(int)wave.type];
+        Enemy prefabE = prefab.GetComponent<Enemy>();
         float delay = wave.timeBetweenEach;
         for(int i = 0; i < wave.nb; i++)
         {
             float spawnPosOffset = Random.Range(-1.0f, 1.0f);
-            GameObject newE = Instantiate(gameplayManager.enemyPrefabs[(int) wave.type], transform.position + spawnOffsetDirection * spawnPosOffset, Quaternion.identity);
+            Vector3 spawnPos = transform.position + spawnOffsetDirection * spawnPosOffset;
+            GameObject newE = Instantiate(prefab, spawnPos, Quaternion.identity);
+            Debug.Log("Spawned AT: " + spawnPos);
+            if (prefabE.flying)
+            {
+                Vector3 flyingPos = newE.transform.GetChild(0).position;
+                flyingPos.y += Random.Range(3f, 4f);
+                newE.transform.GetChild(0).position = flyingPos;
+                Debug.Log("FlyingPos : " + newE.transform.GetChild(0).position);
+            }
+
+            
             yield return delay;
         }
     }
