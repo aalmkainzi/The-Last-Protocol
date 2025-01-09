18284755bbe2efaa29f2cc7b2cc019f8f2a3dd5b
amira allqabba <75005883+amerahxx@users.noreply.github.com>
2025-01-09 16:50:09 +0300

    amira-update

---
+++ Assets/Mechanical Spider/Editor/ApplyMaterialToGroup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SetMaterial))]
+public class ApplyMaterialToGroup : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SetMaterial setMaterial = (SetMaterial)target;
+
+        if (GUILayout.Button("Apply Material"))
+        {
+            Debug.Log("Material Set");
+            setMaterial.ApplyMaterial();
+        }
+    }
+}
+
